24f0f2c69fdc2c5c8cd83f0914f360e9f9506c01
nvallinoto <119407762+nvallinoto@users.noreply.github.com>
2024-11-24 18:14:04 +0100

    Update download_and_search.php

--- download_and_search.php
+++ download_and_search.php
@@ -3,6 +3,9 @@ import json
 import csv
 import re
 import pandas as pd 
+import datetime
+import os
+from IPython.display import HTML
 ALBO_URL = "https://alboonline.comune.genova.it/albopretorio/dispatcher/alboPretorioServlet/invoke"
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 payload = 'dataInputXml=<?xml version="1.0" encoding="UTF-8" ?><filtriAlboPretorio><filtroAlbo><name>TIPO_DOC</name><value /></filtroAlbo><filtroAlbo><name>NASCONDI_ANNULLATE</name><value>VALIDA</value></filtroAlbo><filtroAlbo><name>FULL_TEXT</name><value /></filtroAlbo></filtriAlboPretorio>&nameService=search'
@@ -16,7 +19,7 @@ search_terms = ['stazione marittima', 'funivia', 'espropri', 'forte begato', 'la
 with open('albo.json') as json_file:
     data = json.load(json_file) 
 albo_data = data['data']
-# rimuove colonna esecutivoDal
+# rimuove colonna esecutivoDal presente solo in alcune righe
 for index, doc in enumerate(albo_data):
     if 'esecutivoDal' in doc:
         del doc['esecutivoDal']
@@ -39,17 +42,23 @@ for atto in albo_data:
     if re.search(search_pattern, row[7]):
         csv_writer.writerow(row)
 data_file.close()
-# rimuove colonne inutili
 columns_to_be_removed = ['idUdRettifica','tsPubblicazione','motivoAnnullamento','formaPubblicazione','idDocType','dataAtto','statoPubblicazione','tipo','flgImmediatamenteEsegiubile']
 df = pd.read_csv('albo.csv').drop(columns_to_be_removed, axis = 'columns') 
+pd.set_option('display.max_colwidth', None)
 # crea una nuova colonna con il link all'atto
 df['urlAtto'] = "https://alboonline.comune.genova.it/albopretorio/#/albo/atto/" + df['idUd'].map(str) + "/" + df['idPubblicazione']
-#df['linkAtto'] = '<a href=' + https://www.w3schools.com + '>Visit W3Schools</a>'
-columns_to_be_removed_after = ['idUd','idPubblicazione']
+def make_clickable(val):
+    return '<a target="_blank" href="{}">ATTO</a>'.format(val)
+df.style.format({'urlAtto': make_clickable})
+columns_to_be_removed_after = ['idUd','idPubblicazione','richiedente']
 df = df.drop(columns_to_be_removed_after, axis = 'columns') 
 df.head()  
 # Convert the DataFrame to an HTML table 
-html_table = df.to_html(border=10, index=False, justify='left', col_space=100) 
-# Save the HTML table to a file 
+html_table = df.to_html(border=10, index=False, justify='left', col_space=100, render_links=True, notebook=True)
+# Save the HTML table to a file and rename it with date format 
 with open('albo.html', 'w') as f: 
     f.write(html_table)
+f.close
+dt = str((datetime.datetime.now()).strftime('%Y%m%d'))
+newname = 'albo_'+dt+'.html'
+os.replace('albo.html', newname)
