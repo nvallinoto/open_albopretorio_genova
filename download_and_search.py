9bc5c86a9a01b3ad25acda4c564260724e351141
nvallinoto <119407762+nvallinoto@users.noreply.github.com>
2024-11-30 10:59:13 +0100

    Update download_and_search.py
    aggiunta la gestione dei parametri da riga di comando:
    argv[1] = csv oppure html (il formato del file di output)
    argv[2:] = parole chiave (da ricercare nel campo oggetto dell'albo)

--- download_and_search.py
+++ download_and_search.py
@@ -6,17 +6,31 @@ import pandas as pd
 import datetime
 import os
 from datetime import datetime
-
+import sys
 # config
 ALBO_URL = "https://alboonline.comune.genova.it/albopretorio/dispatcher/alboPretorioServlet/invoke"
 TEMP_DIR = "temp"
 PUB_DIR = "pub"
-SEARCH_TERMS = ['stazione marittima', 'funivia', 'espropri', 'forte begato', 'lagaccio', 'gavoglio', 'principe', 'Doppelmayr', 'Collini', 'B31B21006780001', '9219018E4F', 'MOGE 20792', 'OBR', 'A002D44B88', 'collegamento funiviario', 'interferenze' , 'pnc']
+RECURRENT_SEARCH_TERMS = []
+# RECURRENT_SEARCH_TERMS = ['cantiere','lavori','ecc']
+# argv[1] = filetype (csv/html)
+# argv[2:] = parole chiave
+if len(sys.argv) < 2: 
+    download_typefile = "html"
+    SEARCH_TERMS = []
+else:
+    download_typefile = sys.argv[1]
+    SEARCH_TERMS = sys.argv[2:]    
+if (download_typefile != 'csv' and download_typefile != 'html'):
+    print("Indicare il formato del file di output: csv oppure html")
+    sys.exit()
+SEARCH_TERMS = SEARCH_TERMS + RECURRENT_SEARCH_TERMS 
 
 today = datetime.today().strftime('%Y%m%d')
 json_file = "{}/albo_{}.json".format(TEMP_DIR, today)
 csv_file = "{}/albo_{}.csv".format(TEMP_DIR, today)
 html_file = "{}/albo_{}.html".format(PUB_DIR, today)
+csv_pub_file = "{}/albo_{}.csv".format(PUB_DIR, today)
 
 # Query API and save response as temporary JSON
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -24,7 +38,7 @@ payload = 'dataInputXml=<?xml version="1.0" encoding="UTF-8" ?><filtriAlboPretor
 req = requests.post(ALBO_URL, data = payload, headers=headers)
 req.raise_for_status() # ensure we notice bad responses
 response = req.text
-if response.status_code != 200:
+if req.status_code != 200:
     raise Exception(
         f"Error getting data from {link}. Status code: {response.status_code}"
     )
@@ -63,29 +77,32 @@ for atto in albo_data:
         csv_writer.writerow(row)
 data_file.close()
 
-# Convert to HTML
+# Rimuovi colonne inutili e crea colonna con il link all'atto
 columns_to_be_removed = ['idUdRettifica','tsPubblicazione','motivoAnnullamento','formaPubblicazione','idDocType','dataAtto','statoPubblicazione','tipo','flgImmediatamenteEsegiubile']
 df = pd.read_csv(csv_file).drop(columns_to_be_removed, axis = 'columns')
 pd.set_option('display.max_colwidth', None)
 # crea una nuova colonna con il link all'atto
 df['urlAtto'] = "https://alboonline.comune.genova.it/albopretorio/#/albo/atto/" + df['idUd'].map(str) + "/" + df['idPubblicazione']
-
 def make_clickable(val):
     return '<a target="_blank" href="{}">ATTO</a>'.format(val)
-
 df.style.format({'urlAtto': make_clickable})
-
 columns_to_be_removed_after = ['idUd','idPubblicazione','richiedente']
 df = df.drop(columns_to_be_removed_after, axis = 'columns') 
 
-# Convert the DataFrame to an HTML table 
-html_table = df.to_html(border=10, index=False, justify='left', col_space=100, render_links=True, notebook=True)
-
-# Save the HTML table
-with open(html_file, 'w') as f: 
-    f.write(html_table)
-f.close
-
+if download_typefile == "html":
+    # Convert the DataFrame to an HTML table 
+    html_table = df.to_html(border=10, index=False, justify='left', col_space=100, render_links=True, notebook=True)
+    # Save the HTML table
+    with open(html_file, 'w') as f: 
+        f.write(html_table)
+    f.close
+elif download_typefile == "csv":
+    # Convert the DataFrame to a CSV file 
+    csv_table = df.to_csv()
+    # Save the CSV file
+    with open(csv_pub_file, 'w', newline='',encoding='utf-8') as f: 
+        f.write(csv_table)
+    f.close
 # clear up
 os.remove(json_file)
 os.remove(csv_file)
