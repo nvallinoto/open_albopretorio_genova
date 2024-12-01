649abcae3245e22049d2f42a8e97878d3912c7c2
nvallinoto <119407762+nvallinoto@users.noreply.github.com>
2024-12-01 22:49:58 +0100

    Create view_last_files.py

---
+++ view_last_files.py
@@ -0,0 +1,13 @@
+import os
+directory = 'pub'
+filenames = []
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if filename.endswith(".html") :
+        filenames.append(filename)
+filenames.sort(reverse=True)
+i=0
+for filename in filenames:
+    if i == 15: break
+    print(filename)
+    i=i+1
